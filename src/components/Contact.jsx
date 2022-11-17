15c240d7e0eeeb364cb4d80104df20387afb53e6
Julian Moreyra <julianmoreyra.192@gmail.com>
2022-11-17 08:25:53 -0300

    first commit III

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
@@ -8,8 +8,22 @@ import WorkIcon from '@mui/icons-material/Work';
 import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import Bounce from 'react-reveal/Bounce';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_4xgi5ue', 'template_lxfuhqe', form.current, '4C94SMO99aBAWRtCp')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <div name='contact' className='w-full h-screen bg-[#f6f5e2] flex justify-center items-center p-4 pt-20'>
       <Bounce>
@@ -50,19 +64,21 @@ const Contact = () => {
             </ListItem>
           </List>
         </div>
-        <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
+
+        <form ref={form} onSubmit={sendEmail} className='flex flex-col max-w-[600px] w-full'>
           <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-orange-200 text-stone-800'>Contactenos</p>
             {/* <p className='text-[#829e7e] py-4'>Submit the form below or shoot me an email - julianmoreyra.192@gmail.com</p> */}
           </div>
-          <input className='bg-[#f1f2f3] p-2' type="text" placeholder='Name' name='name' />
-          <input className='my-4 p-2 bg-[#f1f2f3]' type="email" placeholder='Email' name='email' />
+
+          <input className='bg-[#f1f2f3] p-2' type="text" placeholder='Name' name='user_name' />
+          <input className='my-4 p-2 bg-[#f1f2f3]' type="email" placeholder='Email' name='user_email' />
           <textarea className='bg-[#f1f2f3] p-2' name="message" rows="10" placeholder='Message'></textarea>
-          <button className='text-white border-2 hover:bg-orange-200 hover:border-orange-200 px-4 py-3 my-8 mx-auto flex items-center'>Enviar</button>
+          <input type="submit" value="Enviar"  className="text-white border-2 hover:bg-orange-200 hover:border-orange-200 px-4 py-3 my-8 mx-auto flex items-center" />
         </form>
       </Bounce>
     </div>
-  )
-}
+  );
+};
 
 export default Contact
